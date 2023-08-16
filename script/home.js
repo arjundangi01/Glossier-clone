953eb03146664685188caf18d6e0fc6aff9956ef
arjundangi01 <arjundangi907473@gmail.com>
2023-08-16 13:48:43 +0530

    changes8

--- script/home.js
+++ script/home.js
@@ -10,11 +10,12 @@ window.reduceQuantity = reduceQuantity;
 window.addQuantity = addQuantity;
 window.deleteItem = deleteItem;
 // window.hover = hover;
-document.getElementById("navbarId").innerHTML = navbar();
+document.getElementById("navbar").innerHTML = navbar();
 
 let clip = document.querySelectorAll(".video1");
 for (let i = 0; i < clip.length; i++) {
   clip[i].addEventListener("mouseover", function (e) {
+    console.log("first")
     clip[i].play();
   });
   clip[i].addEventListener("mouseout", function (e) {
@@ -30,42 +31,42 @@ insta.addEventListener("click", function () {
 
 // --------------------cart functionality------------
 
-// let cartBtn = document.getElementById("cart-btn");
-// cartBtn.addEventListener("click", () => {
-//   showCart();
-// });
+let cartBtn = document.getElementById("cart-btn");
+cartBtn.addEventListener("click", () => {
+  showCart();
+});
 
-// function reduceQuantity(index) {
-//   let cartArr = JSON.parse(localStorage.getItem("cartArr")) || [];
-//   let ele = cartArr[index];
-//   if (ele.quantity <= 1) {
-//     cartArr.splice(index, 1);
-//     localStorage.setItem("cartArr", JSON.stringify(cartArr));
+function reduceQuantity(index) {
+  let cartArr = JSON.parse(localStorage.getItem("cartArr")) || [];
+  let ele = cartArr[index];
+  if (ele.quantity <= 1) {
+    cartArr.splice(index, 1);
+    localStorage.setItem("cartArr", JSON.stringify(cartArr));
 
-//     displayCart(cartArr);
-//     return;
-//   }
-//   ele.quantity--;
-//   localStorage.setItem("cartArr", JSON.stringify(cartArr));
-//   displayCart(cartArr);
-// }
-// function addQuantity(index) {
-//   let cartArr = JSON.parse(localStorage.getItem("cartArr")) || [];
-//   let ele = cartArr[index];
-//   ele.quantity++;
-//   localStorage.setItem("cartArr", JSON.stringify(cartArr));
-//   displayCart(cartArr);
-// }
-// function deleteItem(index) {
-//   let cartArr = JSON.parse(localStorage.getItem("cartArr")) || [];
-//   cartArr.splice(index, 1);
-//   localStorage.setItem("cartArr", JSON.stringify(cartArr));
+    displayCart(cartArr);
+    return;
+  }
+  ele.quantity--;
+  localStorage.setItem("cartArr", JSON.stringify(cartArr));
+  displayCart(cartArr);
+}
+function addQuantity(index) {
+  let cartArr = JSON.parse(localStorage.getItem("cartArr")) || [];
+  let ele = cartArr[index];
+  ele.quantity++;
+  localStorage.setItem("cartArr", JSON.stringify(cartArr));
+  displayCart(cartArr);
+}
+function deleteItem(index) {
+  let cartArr = JSON.parse(localStorage.getItem("cartArr")) || [];
+  cartArr.splice(index, 1);
+  localStorage.setItem("cartArr", JSON.stringify(cartArr));
 
-//   displayCart(cartArr);
-// }
+  displayCart(cartArr);
+}
 
-// debounceALl();
-// displayUser();
+debounceALl();
+displayUser();
 let allSkin = document.querySelector("#seeSkin");
 allSkin.addEventListener("click", function () {
   window.location.href = "./Product-page/product.html";
@@ -117,13 +118,13 @@ let productImgs = document.querySelectorAll(".product");
 productImgs.forEach((ele,index) => {
   ele.addEventListener("click", () => {
     // event.preventDefault();
-    moveToDetailPage(index)
+    moveToDetailPage3(index)
   })
 })
 
-function moveToDetailPage(index) {
+function moveToDetailPage3(index) {
   let singleItem = arr[index]
   localStorage.setItem("singleItem", JSON.stringify(singleItem));
-  // window.location.assign("../product-detail.html");
+  window.location.assign("../product-detail.html");
   console.log(singleItem)
 }
